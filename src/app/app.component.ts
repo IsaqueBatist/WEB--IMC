4b7594d1a2f516fa9d320c68df4e780b723927b8
Isaque Barbosa <isaquebatista400@gmail.com>
2025-03-27 11:42:40 -0300

    :sparkles: feat: Finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,11 +6,26 @@ import { Component } from '@angular/core';
   standalone: false,
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'projeto3IMC';
   imagem: string = ""
 
+  ngOnInit(): void {
+    this.verificarImagem()
+  }
+
   atualizarImagem(url: string) {
     this.imagem = url;
+    console.log(this.imagem)
+    this.verificarImagem()
+  }
+
+  verificarImagem(){
+    const elemento = document.getElementById('imagemTag')
+    if(this.imagem === ''){
+      elemento?.classList.add('d-none')
+    }else {
+      elemento?.classList.remove('d-none')
+    }
   }
 }
--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -11,21 +11,32 @@ export class FormComponent {
   altura: number = 0
   resultado: number = 0
   resultadoIMC: string = ""
-  imagem: string = "happycat.jpg"
+  imagem: string = ""
 
   @Output() imagemSelecionada = new EventEmitter<string>()
 
   calcularIMC() {
-    this.resultado = parseFloat((this.peso / Math.pow(this.altura, 2)).toFixed(2));
+    if (this.peso <= 0 || this.altura <= 0) {
+      this.imagem=""
+      this.imagemSelecionada.emit(this.imagem)
+      this.resultadoIMC = ''
+      this.resultado = 0
+      return
+    };
+
+    this.resultado = this.peso / (this.altura * this.altura);
+    if(this.resultado > 0 && this.resultado <1) this.resultado *= 10000
+
+    this.resultado = parseFloat(this.resultado.toFixed(2))
     this.resultadoIMC = this.verificarIMC()
   }
 
   verificarIMC(){
-    if (this.resultado < 18.5) {
+    if (this.resultado <= 18.5) {
       this.imagem = "pesoAbaixo.jpg"
       this.imagemSelecionada.emit(this.imagem);
       return 'Abaixo-do-peso';
-    } else if (this.resultado >= 18.5 && this.resultado <= 24.9) {
+    } else if (this.resultado > 18.5 && this.resultado <= 24.9) {
       this.imagem = "pesoNormal.png"
       this.imagemSelecionada.emit(this.imagem);
       return 'Peso-normal';
@@ -33,12 +44,19 @@ export class FormComponent {
       this.imagem = "acimaPeso.jpeg"
       this.imagemSelecionada.emit(this.imagem);
       return 'Sobrepeso';
-    } else if (this.resultado >29.9){
-      this.imagem = "obesidade.jpg"
+    } else if (this.resultado >= 30 && this.resultado <= 34.9){
+      this.imagem = "obesidadegrauI.jpg"
       this.imagemSelecionada.emit(this.imagem);
-      return 'Obesidade';
+      return 'Obesidade grau  I';
+    }else if (this.resultado > 34.9 && this.resultado <=39.9){
+      this.imagem = "obesidadegrauII.jpg"
+      this.imagemSelecionada.emit(this.imagem);
+      return 'Obesidade grau II'
     }else {
-      return ''
+      this.imagem = "obesidadegrauIII.jpg"
+      this.imagemSelecionada.emit(this.imagem);
+      return 'obesidade grau III'
+    }
     }
   }
-}
+
